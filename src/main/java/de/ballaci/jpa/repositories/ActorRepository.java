1437ee65348f9909dc98fb14a87abccd218c6e50
Armand Ballaci <aballaci@gmail.com>
2020-05-11 03:03:49 +0200

    custom query

--- src/main/java/de/ballaci/jpa/repositories/ActorRepository.java
+++ src/main/java/de/ballaci/jpa/repositories/ActorRepository.java
@@ -2,8 +2,22 @@ package de.ballaci.jpa.repositories;
 
 import de.ballaci.jpa.domain.Actor;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
+/**
+ * Accessing JPA Data with REST
+ * https://spring.io/guides/gs/accessing-data-rest/
+ */
 @RepositoryRestResource(collectionResourceRel = "actors", path = "actors")
 public interface ActorRepository extends PagingAndSortingRepository<Actor, Long> {
+    /**
+     * Custom query: Custom queries can be listed in:
+     * http://localhost:8080/actors/search
+     * @param name
+     * @return a list of Actors filtered by lastname
+     */
+    List<Actor> findByLastName(@Param("name") String name);
 }
